4384baeeb947567dfa1d337c732c4a144781536a
Misakura Rin <abcdhsud@gmail.com>
2021-06-15 07:11:09 +0200

    refactor(home): Make homepage look nicer

--- src/components/HomepageHeader/index.tsx
+++ src/components/HomepageHeader/index.tsx
@@ -22,19 +22,18 @@ const HomepageFeature = () => {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title" style={{marginBottom: 30 + "px"}}>{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className={clsx("hero__subtitle", styles.tagLine)}>
+          <img className={clsx(styles.logo)} src="/img/logo.svg"></img>
+          <span className={clsx(styles.span)}>Mapping Tools is an <b>all-in-one</b> application for <b>efficient</b> and <b>creative</b> mapping in osu</span>
+        </h1>
         <div className={clsx("row", styles.buttonsContainer)}>
           <div className={styles.buttons}>
-            <Link
-              className="button button--outline button--secondary button--lg"
-              to={useBaseUrl('docs/')}
-            >
+            <Link className={clsx("button button--secondary button--lg", styles.introButton)} to={useBaseUrl('docs/')}>
               Get Started
             </Link>
           </div>
           <div className={styles.buttons}>
-            <Link className={clsx('button button--outline button--primary button--lg', styles.downloadButton)} to={useBaseUrl('download/')}>
+            <Link className={clsx('button button--primary button--lg', styles.downloadButton)} to={useBaseUrl('download/')}>
               <FontAwesomeIcon icon={faDownload} />&nbsp;Download
             </Link>
           </div>
